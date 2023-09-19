a71409ad0d109875558b988db39a048bd07402b4
Taufik Januar <taufikjanuar35@gmail.com>
2023-09-19 11:27:47 +0700

    fix (LP-05) : fixing migration table payment_channel & transaction

--- db/migrations/20230918031553_create_table_transaction.up.sql
+++ db/migrations/20230918031553_create_table_transaction.up.sql
@@ -4,8 +4,8 @@ CREATE TYPE transaction_type AS ENUM ('TOPUP','PAYOUT');
 CREATE TABLE IF NOT EXISTS transaction (
     id BIGINT NOT NULL PRIMARY KEY,
     customer_id BIGINT NOT NULL,
-    merchant_id BIGINT NOT NULL,
-    payment_channel_id BIGINT NOT NULL,
+    merchant_id BIGINT NULL,
+    payment_channel_id BIGINT NULL,
     ref_id VARCHAR NULL,
     amount INT NOT NULL,
     status transaction_status NOT NULL,
--- db/seeders/insert_data_payment_channel.sql
+++ db/seeders/insert_data_payment_channel.sql
@@ -1,8 +1,8 @@
-INSERT INTO payment_channel (id,name,status) 
+INSERT INTO payment_channel (id,name,code,image_url,payment_instruction,status) 
 VALUES
-(7109387502540034048,'bca','ACTIVE'),
-(7109387502540034049,'bni','ACTIVE'),
-(7109387502540034050,'bri','ACTIVE'),
-(7109387502540034051,'permata','ACTIVE'),
-(7109387502540034052,'mandiri','ACTIVE'),
-(7109387502540034053,'cimb','ACTIVE');
+(7109387502540034048,'Bank BCA','bca','https://iili.io/JHbSf5l.png','','ACTIVE'),
+(7109387502540034049,'Bank BNI','bni','https://iili.io/JHbSlX1.png','','ACTIVE'),
+(7109387502540034050,'Bank BRI','bri','https://iili.io/JHbSg2f.png','','ACTIVE'),
+(7109387502540034051,'Bank Permata','permata','https://iili.io/JHbSDrb.png','','ACTIVE'),
+(7109387502540034052,'Bank Mandiri','mandiri','https://iili.io/JHbUB3J.png','','ACTIVE'),
+(7109387502540034053,'Bank CIMB Niaga','cimb','https://iili.io/JHbUzTN.png','','ACTIVE');
